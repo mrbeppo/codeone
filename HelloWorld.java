627ef539f542b83bf4302da35bbc14e27387098a
Rafael Luty <rl@luty.de>
2018-12-30 22:50:17 +0100

    Hello-World
    Wayne's World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-
-
-  for (int i=0;i<=5;i++) {
-  // Added comment Hello World
-  System.out.println("Hello World! #" + i);
+    for (int i=0;i<=5;i++) {
+    // Added comment Hello World
+    System.out.println("Wayne's World! #" + i);
     }
   }
 
