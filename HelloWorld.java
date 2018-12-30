29086b832d32c00ef162b2afb4f9b417aa1462ae
Rafael Luty <rl@luty.de>
2018-12-30 03:56:59 +0100

    for-Schleife hinzugefügt
    for-Schleife hinzugefügt mit zählender Ausgabe von Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
   public static void main(String[] args) {
 
+
+  for (int i=0;i<=5;i++) {
   // Added comment Hello World
-  System.out.println("Hello World!");
-    
+  System.out.println("Hello World! #" + i);
+    }
   }
 
 }
